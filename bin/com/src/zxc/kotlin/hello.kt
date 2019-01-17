79bd55eef3eb5dec657d3e07f29034ec040d59a5
zxc <842241508@qq.com>
2019-01-17 22:33:19 +0800

    zxc

---
+++ bin/com/src/zxc/kotlin/hello.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+	println("Hello , world");
+	class zxc()
+
+	fun zxc(qwe: String) {
+		qwe.toString();
+		qwe + "String test";
+		println(qwe);
+	}
+
+	var max: Int = 2
+	var a: Int = 1
+	if (max > a) {
+		max = a
+	} else {
+		max = 3
+	}
+	println(max)
+
+}
+
+
